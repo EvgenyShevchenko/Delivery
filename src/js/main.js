4712f2dbe326e79df45f47f897d72ecf811280da
Evgeny Shevchenko <khuivas@yandex.ru>
2020-04-29 00:52:07 +0300

    add modal

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,21 @@
-$(function(){
+$(function () {
+    $('.js-button-cart').click(function () {
+        $('.js-overlay-cart').fadeIn();
+        $('.js-overlay-cart').addClass('disabled');
 
+    });
+
+    $('.js-close-cart,.modal-cart__button').click(function () {
+        $('.js-overlay-cart').fadeOut();
+
+    });
+
+    // закрыть по клику вне окна
+    $(document).mouseup(function (e) {
+        var popup = $('.js-modal-cart');
+        if (e.target != popup[0] && popup.has(e.target).length === 0) {
+            $('.js-overlay-cart').fadeOut();
+
+        }
+    });
 });
